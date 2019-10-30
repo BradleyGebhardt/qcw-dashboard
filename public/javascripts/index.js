e04b58f993a83c47758b2223e2426290dc0d54c5
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2019-10-30 20:39:42 +0200

    Implemented chart.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -18,4 +18,23 @@ async function findAll() {
         });
 }
 
-console.log(findAll());
+// console.log(findAll());
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
