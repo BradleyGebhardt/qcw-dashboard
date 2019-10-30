56acc6579616b1866ac6305fdb4e06d9356a8cdc
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2019-10-30 13:41:18 +0200

    Prototyping the finding of records

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var Scan = require('../models/scan');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // executeFind().then(documents => {
+  //   console.log(documents);
+  // });
+  res.render('index');
 });
+
+function findAll() {
+  return Scan.find().limit(1000).cursor();
+}
+
+async function executeFind() {
+  const docCursor = await findAll();
+  let docs = [];
+  await docCursor.eachAsync(doc => {
+    docs.push(doc);
+  });
+  return docs;
+}
 
 module.exports = router;
