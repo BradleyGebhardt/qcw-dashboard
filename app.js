df20ac6d7899e0cc4a720cd9ec7baa8c883d9402
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2019-10-30 18:35:24 +0200

    Added api route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/apiCalls');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(function (req, res, next) {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
