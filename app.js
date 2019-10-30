87b2c92382ad83ca12fa1ccdd7067223cdb02b94
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2019-10-30 20:40:14 +0200

    Fixed indentation

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Express-session middleware
 app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
 }));
 
 app.use(function (req, res, next) {
-  res.locals.session = req.session;
-  next();
+    res.locals.session = req.session;
+    next();
 });
 
 app.use('/', indexRouter);
@@ -39,18 +39,18 @@ app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
