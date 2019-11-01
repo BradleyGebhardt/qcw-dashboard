817cfd9d3ab3f81a09233a1fb8795c38dd11c12a
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2019-11-01 08:16:40 +0200

    Implemented the methods required to fetch api data

---
+++ public/javascripts/dbData.js
@@ -0,0 +1,30 @@
+async function fetchData() {
+    await fetch('http://localhost:3000/api/mac', { cache: 'force-cache' })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            populateList(data);
+        });
+}
+
+async function findByMac(macAddress) {
+    await fetch(`http://localhost:3000/api/mac/${macAddress}`, { cache: 'force-cache' })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(macAddress + ' Time difference: ' + new Date(new Date(data[data.length - 1].time).getTime() - new Date(data[0].time).getTime()));
+            // console.log(new Date(data[data.length - 1].time) + ' <= => ' + new Date(data[0].time));
+        });
+}
+
+function populateList(data) {
+    // let list = $('.list_macs');
+    // data.forEach(doc => {
+    //     findByMac(doc);
+    // });
+    for (let i = 0; i < 60; i++) {
+        findByMac(data[i]._id.mac);
+    }
+}
