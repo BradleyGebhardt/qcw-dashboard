b399163ad2d1da6f3a02962102e8d5d6e43b9910
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2019-11-01 08:16:04 +0200

    Improved the api calls

--- routes/apiCalls.js
+++ routes/apiCalls.js
@@ -2,30 +2,26 @@ const express = require('express');
 const router = express.Router();
 const Scan = require('../models/scan');
 
-router.get('/manu', async (req, res) => {
+router.get('/mac', async (req, res) => {
     const docs = await Scan.aggregate([
         {
             $group: {
                 _id: {
-                    'manufacturer': '$manu'
+                    'mac': '$mac'
                 }
             }
         }
-    ]).limit(20);
+    ]);
     res.send(docs);
 });
 
-router.get('/all', async (req, res) => {
-    let data = await executeFind(20);
+router.get('/mac/:mac', async (req, res) => {
+    let data = await executeFind(req.params.mac);
     res.send(data);
 });
 
-async function executeFind(num) {
-    if (num) {
-        var docCursor = Scan.find().limit(num).cursor();
-    } else {
-        var docCursor = Scan.find().cursor();
-    }
+async function executeFind(mac) {
+    var docCursor = Scan.find({ mac: mac }).sort({ time: 1 }).cursor();
     let docs = [];
     await docCursor.eachAsync(doc => {
         docs.push(doc);
