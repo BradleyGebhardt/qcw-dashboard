1d81693f8cda72b5d52c810048c7f3ff3560eb6c
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2019-11-03 16:22:17 +0200

    Added more menu controls

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,10 +1,19 @@
 $(document).ready(() => {
-    let navGroups = $('.nav-group');
-    let navItems = navGroups.children();
-    navItems.toggleClass('nav-group-active');
+    const navGroups = $('.nav-group');
+    const navItems = navGroups.children();
+    const sections = $('.section');
+    // navItems.toggleClass('nav-group-active');
+    $('.section.section-overall').toggleClass('display');
 
     navGroups.on('click', function (event) {
         // console.log($(event.target).children());
-        $(event.target).children().toggleClass('nav-group-active');
+        $(event.target).children('.nav-group-item').toggleClass('nav-group-active');
+        $(event.target).children('.nav-arrow').toggleClass('nav-arrow-active');
+    });
+
+    navItems.on('click', function () {
+        sections.removeClass('display');
+        let section = $(`.section.section-${$(this).data('target')}`);
+        section.toggleClass('display');
     });
 });
